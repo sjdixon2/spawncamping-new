876e4448b6718d7104ddac9d23445a1ad564018b
jpnauta <jeremypnauta@gmail.com>
2014-02-17 23:03:48 -0700

    Second part of schema design

--- app.js
+++ app.js
@@ -6,14 +6,6 @@ var http = require('http');
 //Load routes configurations
 require('./routes/config.js');
 
-db
-    .sequelize
-    .complete(function (err) {
-        if (err) {
-            throw err
-        } else {
-            http.createServer(app).listen(app.get('port'), function () {
-                console.log('Express server listening on port ' + app.get('port') + ' in ' + app.get('env') + ' mode');
-            })
-        }
-    });
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port') + ' in ' + app.get('env') + ' mode');
+});
