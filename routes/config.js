f73f6cb1011aa3ae6fe2f11bd615b9b163c082c6
sjdixon <sjdixon2@gmail.com>
2014-02-25 14:02:11 -0700

    Fixed compiler errors.

--- routes/config.js
+++ routes/config.js
@@ -1,5 +1,24 @@
 var main = require('./'),
-    user = require('./user');
+    user = require('./user'),
+    session = require("./session"),
+    photo = require('./photo'),
+    feed = require("./feed");
 
 app.get('/', main.index);
+//app.get("/", feed.index);
 app.get('/users', user.list);
+app.get("/users/new", user.signupForm);
+//app.get("/users/:id", user.stream);
+//app.get("/users/:id/follow", user.follow)
+//app.get("/users/:id/unfollow", user.unfollow)
+app.post("/users/create", user.register);
+
+app.get("/sessions/new", session.loginForm);
+app.post("/sessions/create", session.attemptLogin);
+
+
+app.get("/feed", feed.index);
+
+app.get("/photos/new", photo.newPhotoForm);
+app.post("/photos/create", photo.upload);
+
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,31 @@
  */
 
 exports.list = function (req, res) {
+    // demo
     res.send("respond with a resource");
 };
+
+exports.signupForm = function (req, res) {
+    // send new user form
+	res.send("User signup form");
+}
+
+exports.register = function (req, res) {
+    // register new user in database
+    res.send("User registered.");
+}
+
+exports.stream = function(req, res) {
+    // get user stream
+    res.send("sent photo stream for user :id")
+}
+
+exports.follow = function(req, res){
+    // follow user stream with :id
+    res.send("following :id stream")
+}
+
+exports.unfollow = function(req, res){
+    res.send("unfollowing :id stream");
+}
+
