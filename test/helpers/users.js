33a975c92abd98771920bbeddd7988819e8d4d52
jpnauta <jeremypnauta@gmail.com>
2014-03-03 13:49:29 -0700

    Fix failing tests with user login mockups

---
+++ test/helpers/users.js
@@ -0,0 +1,17 @@
+/**
+ * Simulates a user login for testing purposes
+ * @param session the (supertest-session) session to simulate for
+ * @return {Promise} a promise resolved after user login
+ */
+exports.login = function (session) {
+    var defer = q.defer();
+
+    Factories.user.basic().then(function (user) {
+        session.post('/sessions/create')
+            .send({username: user.email, password: user.password})
+            .expect(302)
+            .expect('location', '/feed', defer.makeNodeResolver());
+    });
+
+    return defer.promise;
+}
