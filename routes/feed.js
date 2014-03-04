7ecfdd2da3eb519362b00e44e94e1e875b22879e
sjdixon <sjdixon2@gmail.com>
2014-03-03 17:13:21 -0700

    Tested that sql query works.

--- routes/feed.js
+++ routes/feed.js
@@ -1,6 +1,5 @@
 exports.index = function(req, res){
     db.Photo.findAll({
-        where : { id : { ne: 0}}
     }).success(function (photos){
             res.render("feed", {
                 title: 'Feed',
