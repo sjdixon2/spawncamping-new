3658b9830b59a53481484e68e4b6a7764cd2e34d
sjdixon <sjdixon2@gmail.com>
2014-03-04 23:00:36 -0700

    Modified some comments.  Removed ones that weren't needed.  Added one for a subtest I'd like to see someday. x

--- routes/config.js
+++ routes/config.js
@@ -33,8 +33,6 @@ app.post('/bulk/users', bulk.users);
 app.post('/bulk/streams', bulk.streams);
 
 app.use(function(req, res) {
-    // curl https://localhost:4000/notfound -vk
-    // curl https://localhost:4000/notfound -vkH "Accept: application/json"
     res.status(404);
     res.render('errors/404', { title:'404: Page not found', error: '404: Page not found', url: req.url });
     return;
@@ -59,9 +57,6 @@ app.use(function(err, req, res, next){
         case 403:
             statusText = 'Forbidden';
             break;
-//        case 404:
-//            statusText = '404: Page not found';
-//            break;
         case 500:
             statusText = 'Internal Server Error';
             break;
--- test/core/routes/error.js
+++ test/core/routes/error.js
@@ -14,6 +14,7 @@ describe('server routing behavior', function(){
             session.get(path)
                 .expect(404)
                 .expect('location', path);
+                //.expect(session.title=='404: Page not found');
         });
 
         it('direct authenticated users to 404', function(){
