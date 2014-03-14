04ef2178338ceef928607c9ecf5d7af2e4a846ff
sjdixon <sjdixon2@gmail.com>
2014-03-14 12:31:14 -0600

    Wrote commented steps for scenario.

--- test/performance/helpers/scenario.js
+++ test/performance/helpers/scenario.js
@@ -4,3 +4,17 @@
 
 // This is the actual test run by concurrent and follower tests.
 
+exports.runScenario = function(req, res){
+    // login
+
+    // go to feed
+
+    // compute response time measured as follows:
+    // time to first image
+
+    // log all other images?
+
+    // time to last image
+
+    // logout
+}
