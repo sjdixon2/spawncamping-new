36634c7d6a33c4068d701f1606a811c32bf95793
sjdixon <sjdixon2@gmail.com>
2014-03-17 13:43:37 -0600

    Improved the bulk upload a bit; made it so dates are always in the past, set the email specifically so you can actually log into snapgram.

--- test/performance/tests/test_concurrent.js
+++ test/performance/tests/test_concurrent.js
@@ -1,4 +1,5 @@
 var runSet = testHelpers.runSet;
+var scenario = testHelpers.scenario;
 
 describe('concurrency performance tests', function () {
     it('Performs a basic concurrent performance test', function () {
