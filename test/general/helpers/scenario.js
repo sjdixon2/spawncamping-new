6ab595c1436abf3686a5881cea5e701b8d3beef0
sjdixon <sjdixon2@gmail.com>
2014-03-19 09:52:02 -0600

    Callbacks seem to work OK - appear to be returning the context after all the methods are called, but it doesn't look like the sessions are concurrent.

--- test/general/helpers/scenario.js
+++ test/general/helpers/scenario.js
@@ -5,37 +5,46 @@
 
 // This is the actual test run by concurrent and follower tests.
 
-exports.login = function(user, session){
-    return session.post('/sessions/create')
-        .send({username: user.email, password: 'test'})
+exports.login = function(context, callback){
+    console.log("Login @ " + new Date());
+    context['loginTime'] = new Date();
+    context['session'].post('/sessions/create')
+        .send({username: context['user'].email, password: 'test'})
         .expect(302)
         .expect('location', '/feed');
+    return callback(context);
 };
 
-exports.captureMetrics = function(context){
-    context['final'] = new Date();
-    console.log(JSON.stringify(context));
-    return context;
+exports.captureMetrics = function(context, callback){
+    console.log("Capture @ " + new Date());
+    context['captured'] = new Date();
+    console.log("result: " + JSON.stringify(context));
+
+    return callback(context);
 };
 
-exports.logout = function(user, session){
-    return session.get("/sessions/destroy")
+exports.logout = function(context, callback){
+    console.log("Logout @ " + new Date());
+    context['session'].get("/sessions/destroy")
         .send()
-        .expect(200)
+        .expect(404)
         .expect('location', '/sessions/new');
+    return callback(context);
 };
 
 exports.runScenario = function(user){
-    console.log(user);
     var context = {
         user: user,
-        initial : new Date()
+        initial : new Date(),
+        session: new Session()
     };
-    var session = new Session();
-    var promise = q.fcall(exports.login(user, session))
-        .then(exports.logout(user, session))
-        .then(exports.captureMetrics(context))
-        .done();
 
-    return context;
+    return exports.login(context, function(c1){
+        return exports.logout(context, function(c2){
+            return exports.captureMetrics(context, function(c3){
+                console.log("Done callback stack");
+                return c3;
+            })
+        })
+    })
 };
--- test/performance/tests/test_concurrent.js
+++ test/performance/tests/test_concurrent.js
@@ -3,9 +3,8 @@ var scenario = testHelpers.scenario;
 var reporter = testHelpers.reporter;
 
 describe('Response Time due to Number of Concurrent Sessions', function () {
-    var session;
     it('Varies due to scaling number of concurrent sessions', function () {
         return runSet.byConcurrentSessions(4, scenario.runScenario)
-            .then(reporter.print_results)
+            .then(reporter.print_results);
     });
 });
