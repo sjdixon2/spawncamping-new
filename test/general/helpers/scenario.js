fa51451e584517e6ff30b7f94d60a5819dac5b02
sjdixon <sjdixon2@gmail.com>
2014-03-20 16:15:45 -0600

    Before Jeremys skeletoni

--- test/general/helpers/scenario.js
+++ test/general/helpers/scenario.js
@@ -7,42 +7,60 @@ var assert = require('assert');
 var request = require('./request');
 
 exports.login = function(context){
-//    var deferred = q.defer();
-//    console.log("login");
-//    var args = [
-//        "localhost:8800/sessions/create",
-//        {
-//            username: context['user'].email,
-//            password: "test"
-//        },
-//        {
-//            Connection: "keep-alive"
-//        }
-//    ]
-//    request.doRequest("post", args).then(function(resp){
-//        console.log("resp");
-//        context['cookie'] = resp.headers['set-cookie'][0];
-//        //assert.equal(resp.statusCode, 302);
-//        //assert.equal(resp.headers.location, "/feed");
-//        deferred.resolve(context)
-//    })
-//
-//    return deferred.promise;
-    return 10;
+    var data = [
+        "localhost:8800/sessions/create",
+        {
+            username: "test5",
+            password: "test"
+        }
+    ];
+    return request.doRequest("post", data).then(function (resp){
+        console.log("request finished");
+        assert.equal(resp.statusCode, 302);
+        assert.equal(resp.headers.location, "/feed");
+        console.log("assertions passed");
+        context['cookie'] = resp.headers['set-cookie'][0];
+        context['loginTime'] = new Date();
+        return 123;
+    });
+}
+
+
+exports.loadFeed= function(context){
+    console.log("getting feed..");
+    var data = [
+        "localhost:8800/feed",
+        {
+            cookie: context['cookie']
+        },
+        {
+            Connection: "keep-alive"
+        }
+    ]
+    return request.doRequest("get", data).then(function(resp){
+        console.log("rendered");
+        assert.equal(resp.statusCode, 200);
+        //console.log(body);
+        return context;
+    });
+}
+
+exports.logout= function(context){
+    return context;
 }
 
 
 exports.runScenario = function(user){
-    console.log(user);
     var deferred = q.defer();
-
     var context = {
         user: user,
         initial : new Date()
     };
 
-    setTimeout(function(){
-        deferred.resolve(10);
-    }, 1000);
-    return deferred.promise;
+    return exports.login(context)
+        .then(function(c){
+            console.log("got here");
+            return exports.loadFeed(c);
+        })
+        .then(exports.logout);
 };
--- test/performance/tests/test_concurrent.js
+++ test/performance/tests/test_concurrent.js
@@ -29,99 +29,11 @@ describe('Response Time due to Number of Concurrent Sessions', function () {
     }
 
     it('Varies due to scaling number of concurrent sessions', function () {
-//        return runSet.byConcurrentSessions(1, scenario.runScenario)
-//            .then(reporter.print_results);
+        return runSet.byConcurrentSessions(10, scenario.runScenario)
+            .then(reporter.print_results);
     });
 
     it("varies using promises only", function(){
-        var deferred = q.defer();
-        var prom = bulk.generateUsersAndFollowers(5)
-            .then(function(followers){
-                //console.log(followers);
-                return userToTest = _.max(followers, function(follower){
-                    return follower.id;
-                })
-            })
-            .then(function(user){
-                console.log(user);
-                var context = {
-                    user: user,
-                    initial : new Date()
-                }
-                var data = {
-                    username: context['user'].email,
-                    password: 'test'
-                };
-                var options = {
-                    headers:
-                    {
-                        Connection: "keep-alive"
-                    }
-                }
-                var needlePost = q.defer();
-//                testHelpers.request.doRequest('post', ["localhost:8800/sessions/create", data, options]).then(function resp, body) {
-//
-//                }
-                needle.post( function(err,resp,body){
-                    printArguments(err,resp);
-                    if(err){
-                        var error = new Error(err);
-                        needlePost.reject(error);
-                        throw error;
-                    }
-                    else {
-                        context['loggedIn'] = true;
-                        context['loginTime'] = new Date();
-                        assert.equal(resp.statusCode, 302);
-                        assert.equal(resp.headers.location, "/feed");
-                        assert(resp.headers['set-cookie'][0]);
-                        console.log("got 200");
-                        var cookie = resp.headers['set-cookie'][0];
-//                        cookie = cookie.substr(0, cookie.indexOf(';'));
-                        context['loginCookie'] = cookie;
-                        needlePost.resolve(context);
-                    }
-                });
-                return needlePost.promise;
-            })
-            .then(function(loginContext){
-                var deferred = q.defer();
-                var options = {
-                    headers : {
-                        Host: "localhost:8080",
-                        Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                        "Accept-Language": "en-gb,en;q=0.5",
-                        "Accept-Encoding": "gzip, deflate",
-                        DNT: 1,
-                        referer: 'localhost:8800/sessions/new',
-                        "cookie" : loginContext['loginCookie'],
-                        connection : "keep-alive"
-                    }
-//                    username: 'test5',
-//                    password: 'test',
-//                    auth: 'digest'
-                };
-                needle.get("localhost:8800/feed", options, function(err, resp){
-                    printArguments(err, resp);
-                    if(err){
-                        console.log(error);
-                        var error = new Error(err);
-                        deferred.reject(error);
-                        throw error;
-                    }
-                    else {
-                        loginContext['gotFeed'] = new Date();
-                        deferred.resolve(loginContext);
-                        assert.equal(resp.statusCode, 200);
-                        assert.equal(resp.headers.location, "/feed")
-                    }
-                })
-                return deferred.promise;
-            })
-            .then(function(result){
-                console.log(result);
-                deferred.resolve(result);
-            })
-        return deferred.promise;
+
     });
 });
