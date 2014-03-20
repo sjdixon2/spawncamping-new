8a50ef812cad1fa4b2c600ceba55ffda6fed2a10
sjdixon <sjdixon2@gmail.com>
2014-03-20 16:23:58 -0600

    Completed scaffolding for scenario.

--- test/general/helpers/scenario.js
+++ test/general/helpers/scenario.js
@@ -6,61 +6,59 @@ var needle = require('needle');
 var assert = require('assert');
 var request = require('./request');
 
-exports.login = function(context){
+exports.login = function (user) {
     var data = [
         "localhost:8800/sessions/create",
         {
-            username: "test5",
+            username: user.email,
             password: "test"
         }
     ];
-    return request.doRequest("post", data).then(function (resp){
+    return request.doRequest("post", data).then(function (resp) {
         console.log("request finished");
         assert.equal(resp.statusCode, 302);
         assert.equal(resp.headers.location, "/feed");
         console.log("assertions passed");
-        context['cookie'] = resp.headers['set-cookie'][0];
-        context['loginTime'] = new Date();
-        return 123;
+        return resp.headers['set-cookie'][0];
     });
 }
 
 
-exports.loadFeed= function(context){
-    console.log("getting feed..");
+exports.loadFeed = function (cookie) {
     var data = [
         "localhost:8800/feed",
         {
-            cookie: context['cookie']
+            cookie: cookie
         },
         {
             Connection: "keep-alive"
         }
     ]
-    return request.doRequest("get", data).then(function(resp){
+    return request.doRequest("get", data).then(function (resp, body) {
         console.log("rendered");
         assert.equal(resp.statusCode, 200);
-        //console.log(body);
-        return context;
+        return body;
     });
 }
 
-exports.logout= function(context){
-    return context;
+exports.logout = function () {
+    return;
 }
 
+exports.loadImages = function(body){
 
-exports.runScenario = function(user){
-    var deferred = q.defer();
-    var context = {
-        user: user,
-        initial : new Date()
-    };
+}
 
-    return exports.login(context)
-        .then(function(c){
-            console.log("got here");
-            return exports.loadFeed(c);
-        })
-        .then(exports.logout);
+exports.runScenario = function (user) {
+    return exports.login(user).then(function (cookie) {
+        var initial = new Date()
+        return exports.loadFeed(cookie).then(function(body) {
+            return exports.loadImages(body).then(function () {
+                return exports.logout().then(function(){
+                    var endTime = new Date();
+                    return {time: endTime - initial, asd: 1};
+                });
+            });
+        });
+    });
 };
