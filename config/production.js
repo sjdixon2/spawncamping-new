e8773493958f2073525c7645b8f31f842e87cfd7
jpnauta <jeremypnauta@gmail.com>
2014-03-21 19:21:44 -0600

    (revert)

--- config/production.js
+++ config/production.js
@@ -13,27 +13,3 @@ extend(global.settings, {
         }
     }
 });
-
-
-//Database Logging
-if (!fs.existsSync(system.pathTo(settings.LOG_PATH))) { //Create log folder if it doesn't exist
-    fs.mkdirSync(system.pathTo(settings.LOG_PATH));
-};
-
-var queryLog = fs.createWriteStream(system.pathTo(settings.LOG_PATH+'queries.txt'), {
-    flags: 'w'
-});
-
-global.settings.db.options.logging = function(toLog){
-    var date = new Date();
-    queryLog.write(date.getTime() + 'ms : ' +  date.toUTCString() + ' ' + toLog + '\n');
-};
-
-//Write express logs to file
-app.use(express.logger({
-    format: ':date [:remote-addr] [:session] [:user] [:response-time ms] [req::req[Content-Length] res::res[Content-Length]] :method :status :url ',
-    immediate: false,
-    stream: fs.createWriteStream(system.pathTo(settings.LOG_PATH+'requests.txt'), {
-        flags: 'w'
-    })
-}));
